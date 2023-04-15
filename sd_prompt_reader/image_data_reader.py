2e3bee62c13443fa76d56e43a4a969bc9f668b2f
Rhys Yang <receyuki@gmail.com>
2023-04-16 01:45:32 +0800

    Update comfyUI reader

--- sd_prompt_reader/image_data_reader.py
+++ sd_prompt_reader/image_data_reader.py
@@ -91,7 +91,7 @@ class ImageDataReader:
         workflow = self._info.get("workflow") or {}
         prompt_json = json.loads(prompt)
         self._raw += str(prompt)
-
+        print(str(prompt))
         if workflow:
             self._raw += "\n" + workflow
         # for value in prompt_json.values():
@@ -101,58 +101,153 @@ class ImageDataReader:
         # ksampler = next(filter(lambda value: value.get("class_type") in sampler_type, prompt_json.values()), None)
         # multi chain
         end_nodes = list(filter(lambda item: item[-1].get("class_type") == "SaveImage", prompt_json.items()))
-        chains = []
+        # flows = []
+        # node_flows = []
         # print(end_nodes)
-
+        longest_flow = {}
+        longest_nodes = []
+        longest_flow_len = 0
         for end_node in end_nodes:
             chain = {}
-            chain = self._comfy_traverse(prompt_json, str(end_node[0]))
-            print(chain)
-            chains.append(chain)
+            flow, nodes = self._comfy_traverse(prompt_json, str(end_node[0]))
+            # print(chain)
+            # flows.append(chain)
+            # node_flows.append(nodes)
+            if len(nodes) > longest_flow_len:
+                longest_flow = flow
+                longest_nodes = nodes
+                longest_flow_len = len(nodes)
+            print("node", nodes)
+            # print(json.dumps(flow))
 
-        # print(prompt)
+        print(longest_flow)
+
+        self._setting = (
+            f"Seed: {longest_flow.get('seed')}"
+            f", Steps: {longest_flow.get('steps')}"
+            f", CFG scale: {longest_flow.get('cfg')}"
+            f", Sampler: {self.remove_quotes(longest_flow.get('sampler_name'))}"
+            f", Scheduler: {self.remove_quotes(longest_flow.get('scheduler'))}"
+            f", Denoise: {longest_flow.get('denoise')}"
+            f", Size: {self._width}x{self._height}"
+            f", Model: {self.remove_quotes(longest_flow.get('ckpt_name'))}"
+
+        )
+        if "upscale_method" in longest_flow:
+            self._setting += f", Upscale method: {self.remove_quotes(longest_flow.get('upscale_method'))}"
+        if "upscaler" in longest_flow:
+            self._setting += f", Upscale model: {self.remove_quotes(longest_flow.get('upscaler'))}"
 
     def _comfy_traverse(self, prompt, end_node):
         SAMPLES_SKIP_TYPES = ["VAEDecode", "LatentUpscale"]
         KSAMPLER_TYPES = ["KSampler", "KSamplerAdvanced"]
-        chain = {}
-        if prompt[end_node]["class_type"] == "SaveImage":
-            print(end_node)
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["images"][0]))
-        elif prompt[end_node]["class_type"] in KSAMPLER_TYPES:
-            print(end_node)
-            chain = prompt[end_node]["inputs"]
-            branch1 = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["model"][0]) or {}
-            branch2 = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["latent_image"][0]) or {}
-            chain.update(max([branch1, branch2], key=len))
-        elif prompt[end_node]["class_type"] == "LoraLoader":
-            print(end_node)
-            chain = prompt[end_node]["inputs"]
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["model"][0]))
-        elif prompt[end_node]["class_type"] == "CheckpointLoaderSimple":
-            print(end_node)
-            return prompt[end_node]["inputs"]
-        elif prompt[end_node]["class_type"] == "EmptyLatentImage":
-            print(end_node)
-            return
-        elif prompt[end_node]["class_type"] == "VAEEncode":
-            print(end_node)
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["pixels"][0]))
-        elif prompt[end_node]["class_type"] == "ImageScale":
-            print(end_node)
-            chain = prompt[end_node]["inputs"]
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["image"][0]))
-        elif prompt[end_node]["class_type"] == "ImageUpscaleWithModel":
-            print(end_node)
-            chain = prompt[end_node]["inputs"]
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["image"][0]))
-        elif prompt[end_node]["class_type"] in SAMPLES_SKIP_TYPES:
-            print(end_node)
-            chain.update(self._comfy_traverse(prompt, prompt[end_node]["inputs"]["samples"][0]))
+        flow = {}
+        node = []
+        match prompt[end_node]["class_type"]:
+            case "SaveImage":
+                print(end_node)
+                node.append(end_node)
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["images"][0])
+                print("aaaa")
+                print(last_flow)
+                flow = self.merge_dict(flow, last_flow)
+                node += last_node
+                print("aaaa")
+                print(end_node)
+            case node_type if node_type in KSAMPLER_TYPES:
+                print(end_node)
+                node.append(end_node)
+                flow = prompt[end_node]["inputs"]
+                last_flow1, last_node1 = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["model"][0])
+                last_flow2, last_node2 = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["latent_image"][0])
+                self._positive = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["positive"][0])
+                self._negative = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["negative"][0])
+                if len(last_node1 or []) >= len(last_node2 or []):
+                    flow = self.merge_dict(flow, last_flow1)
+                    node += last_node1
+                else:
+                    flow = self.merge_dict(flow, last_flow2)
+                    node += last_node2
+                print(end_node)
+            case "CLIPTextEncode":
+                print(end_node)
+                return prompt[end_node]["inputs"]["text"]
+            case "LoraLoader":
+                print(end_node)
+                node.append(end_node)
+                flow = prompt[end_node]["inputs"]
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["model"][0])
+                flow = self.merge_dict(flow, last_flow)
+                node += last_node
+                print(end_node)
+            case "CheckpointLoaderSimple":
+                print(end_node)
+                node.append(end_node)
+                print(end_node)
+                return prompt[end_node]["inputs"], node
+            case "EmptyLatentImage":
+                print(end_node)
+                node.append(end_node)
+                print(end_node)
+                return {}, node
+            case "VAEEncode":
+                print(end_node)
+                node.append(end_node)
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["pixels"][0])
+                flow = self.merge_dict(flow, last_flow)
+                node += last_node
+                print(end_node)
+            case "ImageScale":
+                print(end_node)
+                node.append(end_node)
+                flow = prompt[end_node]["inputs"]
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["image"][0])
+                flow = self.merge_dict(flow, last_flow)
+                node += last_node
+                print(end_node)
+            case "UpscaleModelLoader":
+                print(end_node)
+                node.append(end_node)
+                print(end_node)
+                return {"upscaler": prompt[end_node]["inputs"]["model_name"]}
+            case "ImageUpscaleWithModel":
+                print(end_node)
+                node.append(end_node)
+                flow = prompt[end_node]["inputs"]
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["image"][0])
+                model = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["upscale_model"][0])
+                flow = self.merge_dict(flow, last_flow)
+                flow = self.merge_dict(flow, model)
+                node += last_node
+                print(end_node)
+            case node_type if node_type in SAMPLES_SKIP_TYPES:
+                print(end_node)
+                node.append(end_node)
+                last_flow, last_node = self._comfy_traverse(prompt, prompt[end_node]["inputs"]["samples"][0])
+                print(flow)
+                print(last_flow)
+                flow = self.merge_dict(flow, last_flow)
+                node += last_node
+                print(end_node)
+        return flow, node
 
-        print(chain)
-        return chain
-    #add latent image
+    @staticmethod
+    def merge_str_to_tuple(item1, item2):
+        if not isinstance(item1, tuple):
+            item1 = (item1,)
+        if not isinstance(item2, tuple):
+            item2 = (item2,)
+        return item1 + item2
+
+    def merge_dict(self, dict1, dict2):
+        dict3 = dict1.copy()
+        for k, v in dict2.items():
+            dict3[k] = self.merge_str_to_tuple(v, dict3[k]) if k in dict3 else v
+        return dict3
+
+    @staticmethod
+    def remove_quotes(string):
+        return str(string).replace('"', '').replace("'", "")
 
     @property
     def positive(self):
