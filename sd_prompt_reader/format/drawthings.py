38dc754e594c6a8b4bd8b3dae738a3b71716a024
receyuki <receyuki@gmail.com>
2023-09-15 04:33:05 +0800

    Refactor drawthings.py

--- sd_prompt_reader/format/drawthings.py
+++ sd_prompt_reader/format/drawthings.py
@@ -3,14 +3,14 @@ __filename__ = "drawthings.py"
 __copyright__ = "Copyright 2023"
 __email__ = "receyuki@gmail.com"
 
-import json
-from xml.dom import minidom
 
 from ..format.base_format import BaseFormat
 from ..utility import remove_quotes
 
 
 class DrawThings(BaseFormat):
+    SETTING_KEY = ["model", "sampler", "seed", "scale", "steps", "size"]
+
     def __init__(self, info: dict = None, raw: str = ""):
         super().__init__(info, raw)
         self._dt_format()
@@ -18,16 +18,12 @@ class DrawThings(BaseFormat):
     def _dt_format(self):
         data_json = self.info
         self._tool = "Draw Things"
-        self._positive = data_json.get("c")
-        self._negative = data_json.get("uc")
+        self._positive = data_json.get("c").strip()
+        self._negative = data_json.get("uc").strip()
         self._raw = "\n".join([self._positive, self._negative, str(data_json)])
         data_json.pop("c")
         data_json.pop("uc")
-        self._setting = remove_quotes(str(data_json)[1:-1])
+        self._setting = remove_quotes(str(data_json)[1:-1]).strip()
 
-        self._parameter["model"] = data_json.get("model")
-        self._parameter["sampler"] = data_json.get("sampler")
-        self._parameter["seed"] = data_json.get("seed")
-        self._parameter["cfg"] = data_json.get("scale")
-        self._parameter["steps"] = data_json.get("steps")
-        self._parameter["size"] = data_json.get("size")
+        for p, s in zip(super().PARAMETER_KEY, DrawThings.SETTING_KEY):
+            self._parameter[p] = data_json.get(s)
