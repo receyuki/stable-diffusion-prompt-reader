c9cd58ced7d27910c63116f5aed8f61b5c01c28a
receyuki <receyuki@gmail.com>
2023-09-15 04:46:45 +0800

    Refactor fooocus.py

--- sd_prompt_reader/format/fooocus.py
+++ sd_prompt_reader/format/fooocus.py
@@ -3,14 +3,21 @@ __filename__ = "fooocus.py"
 __copyright__ = "Copyright 2023"
 __email__ = "receyuki@gmail.com"
 
-import json
-from xml.dom import minidom
 
 from ..format.base_format import BaseFormat
 from ..utility import remove_quotes
 
 
 class Fooocus(BaseFormat):
+    SETTING_KEY = [
+        "base_model",
+        "sampler",
+        "seed",
+        "cfg",
+        "steps",
+        "",
+    ]
+
     def __init__(self, info: dict = None, raw: str = ""):
         super().__init__(info, raw)
         self._fc_format()
@@ -18,18 +25,18 @@ class Fooocus(BaseFormat):
     def _fc_format(self):
         data_json = self.info
         self._tool = "Fooocus"
-        self._positive = data_json.get("prompt")
-        self._negative = data_json.get("negative_prompt")
+        self._positive = data_json.get("prompt").strip()
+        self._negative = data_json.get("negative_prompt").strip()
         self._raw = "\n".join([self._positive, self._negative, str(data_json)])
         data_json.pop("prompt")
         data_json.pop("negative_prompt")
-        self._setting = remove_quotes(str(data_json)[1:-1])
+        self._setting = remove_quotes(str(data_json)[1:-1]).strip()
 
-        self._parameter["model"] = data_json.get("base_model")
-        self._parameter["sampler"] = data_json.get("sampler")
-        self._parameter["seed"] = data_json.get("seed")
-        self._parameter["cfg"] = data_json.get("cfg")
-        self._parameter["steps"] = data_json.get("steps")
-        self._parameter["size"] = (
-            str(data_json.get("width")) + "x" + str(data_json.get("height"))
-        )
+        for p, s in zip(super().PARAMETER_KEY, Fooocus.SETTING_KEY):
+            match p:
+                case "size":
+                    self._parameter["size"] = (
+                        str(data_json.get("width")) + "x" + str(data_json.get("height"))
+                    )
+                case _:
+                    self._parameter[p] = data_json.get(s)
