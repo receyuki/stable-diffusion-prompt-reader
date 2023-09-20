554310ceda823c453ed346019b66322004240a35
receyuki <receyuki@gmail.com>
2023-09-21 02:35:20 +0800

    Refactor novelai.py

--- sd_prompt_reader/format/novelai.py
+++ sd_prompt_reader/format/novelai.py
@@ -6,39 +6,39 @@ __email__ = "receyuki@gmail.com"
 import json
 
 from ..format.base_format import BaseFormat
+from ..utility import remove_quotes
 
 
 class NovelAI(BaseFormat):
+    SETTING_KEY = [
+        "sampler",
+        "sampler_name",
+        "seed",
+        "scale",
+        "steps",
+        "",
+    ]
+
     def __init__(self, info: dict = None, raw: str = ""):
         super().__init__(info, raw)
         self._nai_png()
 
     def _nai_png(self):
-        self._positive = self._info.get("Description")
+        self._positive = self._info.get("Description").strip()
         self._raw += self._positive
-        comment = self._info.get("Comment") or {}
-        comment_json = json.loads(comment)
-        self._negative = comment_json.get("uc")
-        self._raw += "\n" + self.negative
-        self._raw += "\n" + comment
-        self._setting = (
-            f"Steps: {comment_json.get('steps')}"
-            f", Sampler: {comment_json.get('sampler')}"
-            f", CFG scale: {comment_json.get('scale')}"
-            f", Seed: {comment_json.get('seed')}"
-            f", Size: {self._width}x{self._height}"
-        )
-        if "strength" in comment_json:
-            self._setting += f", Strength: {comment_json.get('strength')}"
-        if "noise" in comment_json:
-            self._setting += f", Noise: {comment_json.get('noise')}"
-        if "scale" in comment_json:
-            self._setting += f", Scale: {comment_json.get('scale')}"
-        if self._setting:
-            self._setting += ", Clip skip: 2, ENSD: 31337"
+        data = self._info.get("Comment") or {}
+        data_json = json.loads(data)
+        self._negative = data_json.get("uc").strip()
+        self._raw += "\n".join([self._positive, self.negative, str(data_json)]).strip()
 
-        self._parameter["sampler"] = comment_json.get("sampler")
-        self._parameter["seed"] = comment_json.get("seed")
-        self._parameter["cfg"] = comment_json.get("scale")
-        self._parameter["steps"] = comment_json.get("steps")
-        self._parameter["size"] = str(self._width) + "x" + str(self._height)
+        data_json.pop("uc")
+        self._setting = remove_quotes(str(data_json)).strip("{ }")
+
+        for p, s in zip(super().PARAMETER_KEY, NovelAI.SETTING_KEY):
+            match p:
+                case "model":
+                    self._parameter["model"] = ""
+                case "size":
+                    self._parameter["size"] = str(self._width) + "x" + str(self._height)
+                case _:
+                    self._parameter[p] = str(data_json.get(s))
