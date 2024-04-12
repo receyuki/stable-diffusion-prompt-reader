2cd5ed930ca6b6d6c165bafcde60e90cb86901c7
receyuki <receyuki@gmail.com>
2024-04-13 00:34:48 +0800

    Update pillow to 10.3.0

--- whl/fuse_pillow.sh
+++ whl/fuse_pillow.sh
@@ -1,1 +1,1 @@
-delocate-fuse Pillow-10.2.0-cp311-cp311-macosx_11_0_arm64.whl Pillow-10.2.0-cp311-cp311-macosx_10_10_x86_64.whl -w tmp
+delocate-fuse pillow-10.3.0-cp311-cp311-macosx_11_0_arm64.whl pillow-10.3.0-cp311-cp311-macosx_10_10_x86_64.whl -w tmp
