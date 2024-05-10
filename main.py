381a10ed1afae49792d09d3e38adfbcb6d5157f7
Rhys Yang <receyuki@gmail.com>
2024-05-10 21:17:03 +0800

    Fix CLI/GUI detection mechanism

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from sd_prompt_reader.app import main
 from sd_prompt_reader.cli import cli
 import sys
 
+
+def is_console():
+    return False if sys.stdin is None else sys.stdin.isatty()
+
+
 if __name__ == "__main__":
-    if sys.stdin.isatty():
+    if is_console():
         cli()
     else:
         main()
